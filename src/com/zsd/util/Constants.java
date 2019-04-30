74b3e8307542434d9cca5fac822f6ce97c05aa88
zongdengfang <pyzdf13@163.com>
2019-04-30 10:56:37 +0800

    测试提交 Signed-off-by: zongdengfang <pyzdf13@163.com>

--- src/com/zsd/util/Constants.java
+++ src/com/zsd/util/Constants.java
@@ -17,7 +17,7 @@ public class Constants {
 	public static final String WEB_GRADE_SUBJECT_INFO = "grade_subject_Web";
 	public static final String WEB_EDUCATION_INFO = "education_Web";
 	public static final String WEB_SCHOOL_INFO = "school_Web";
-
+	public static final String WEB_USER_INFO = "user_Web";
 	
 	//-------------------DAO-------------------------//
 	public static final String DAO_EDITION_INFO = "edition_Dao";
@@ -25,4 +25,5 @@ public class Constants {
 	public static final String DAO_GRADE_SUBJECT_INFO = "grade_subject_Dao";
 	public static final String DAO_EDUCATION_INFO = "education_Dao";
 	public static final String DAO_SCHOOL_INFO = "school_Dao";
+	public static final String DAO_USER_INFO ="user_Dao";
 }
