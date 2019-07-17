096244c20c09f7e5baa651362e57b217b50ab5ac
sandywm <270826057@qq.com>
2019-07-17 22:00:59 +0800

    tj Signed-off-by: sandywm <270826057@qq.com>

--- src/com/zsd/dao/impl/GuideStudyDaoImpl.java
+++ src/com/zsd/dao/impl/GuideStudyDaoImpl.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 
 import com.zsd.dao.GuideStudyDao;
 import com.zsd.module.GuideStudyInfo;
+import com.zsd.tools.CommonTools;
 
 @SuppressWarnings("unchecked")
 public class GuideStudyDaoImpl implements GuideStudyDao{
@@ -38,15 +39,48 @@ public class GuideStudyDaoImpl implements GuideStudyDao{
 			Integer guideStatus, Integer guideUserId, String sDate,
 			String eDate, boolean pageFlag, Integer pageNo, Integer pageSize) {
 		// TODO Auto-generated method stub
-		
-		return null;
+		String hql = " from GuideStudyInfo as gs where 1=1";
+		if(stuId > 0){
+			hql += " and gs.studyLogInfo.user.id = "+stuId;
+		}
+		if(guideStatus > 0){
+			hql += " and gs.guideStatus = "+guideStatus;
+		}
+		if(guideUserId > 0){
+			hql += " and gs.guideUserId = "+guideUserId;
+		}
+		if(!sDate.equals("") && !eDate.equals("")){
+			hql += "  and substring(gs.addTaskDate,1,10) >= '"+sDate+"' and substring(gs.addTaskDate,1,10) >= '"+eDate+"'";
+		}
+		if(pageFlag){
+			int offset = (pageNo - 1) * pageSize;
+			if (offset < 0) {
+				offset = 0;
+			}
+			return sess.createQuery(hql).setFirstResult(offset).setMaxResults(pageSize).list();
+		}
+		return sess.createQuery(hql).list();
 	}
 
 	@Override
 	public Integer getCountByOpt(Session sess, Integer stuId,
 			Integer guideStatus, Integer guideUserId, String sDate, String eDate) {
 		// TODO Auto-generated method stub
-		return null;
+		String hql = "select count(gs.id) from GuideStudyInfo as gs where 1=1";
+		if(stuId > 0){
+			hql += " and gs.studyLogInfo.user.id = "+stuId;
+		}
+		if(guideStatus > 0){
+			hql += " and gs.guideStatus = "+guideStatus;
+		}
+		if(guideUserId > 0){
+			hql += " and gs.guideUserId = "+guideUserId;
+		}
+		if(!sDate.equals("") && !eDate.equals("")){
+			hql += "  and substring(gs.addTaskDate,1,10) >= '"+sDate+"' and substring(gs.addTaskDate,1,10) >= '"+eDate+"'";
+		}
+		Object countObj = sess.createQuery(hql).uniqueResult();
+		return CommonTools.longToInt(countObj);
 	}
 
 }
